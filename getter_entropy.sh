fa7499be9ca7feb136cd71f3ff505f151a1c46ec
0x07CB <83157348+0x07CB@users.noreply.github.com>
2021-12-07 12:01:39 +0100

    first push in French sorry

---
+++ getter_entropy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#coding: utf-8
+clear
+echo "      °        °       °°          °       °         °     °   "
+echo "       °  °   °                           °           °  °     "        
+echo "   /  °  °°     °    °  °   °°     ° °      °   °   °   °°\    "
+echo "  /~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\   "
+echo " /  °     °      °°    °  °  °     °       ° °       °  ° ° \  "
+echo "/°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°\ "
+echo "\_____________[*---- $(cat /proc/sys/kernel/random/entropy_avail) ----*]_____________________________/ "
+echo " \............[(+---- NIVEAUX ENTROPIQUE ----+)]............/  "
+echo "  \--------------------------------------------------------/   "
+echo ""
+echo "INFO: Le niveaux entropique est en relation avec de bonnes conditions pour le chiffrements, notamment asymétrique ,ex: SSL,.... Les module TRNG permettent peuvent utiliser les capacités internes, que ce soit une puce ou une spécificité propre au matériel, la plupart des ordinateurs possède cette capacité ...on oublie souvent les raspberry pi qui eux aussi on cette capacité est ce qui est essenciel si vous en faite un serveur et le ne touchez pas niveau périphériques..."
+echo ""
+echo "ATTENTION: SI VOTRE NIVEAU ENTROPIQUE EST EN DESSOUS DE 200 voir ENTRE 100 ET 200."
+echo "           ALORS VOUS AVEZ UN GROS PROBLEME ET VOUS DEVRIEZ UTILISER UN TRNG."
+echo "POUR TOUTES INFOS CONTACTEZ MOI."
+
---
+++ loop-getter_entropy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#coding: utf-8
+while :
+do
+  /opt/getter_entropy.sh
+  sleep 1
+done
